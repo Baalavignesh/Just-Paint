57ec0e1aa8d91a93a02d50251dd2b90dca45318a
Prakash <pk4501020@gmail.com>
2020-07-05 15:37:21 +0530

    download bug fixed

--- src/Reusables/Canvas/Canvas.js
+++ src/Reusables/Canvas/Canvas.js
@@ -1,77 +1,70 @@
 import React, { Component } from 'react'
 import Sketch from 'react-p5'
 import Button from '../Button/Button';
-import p5 from 'p5';
 import './Canvas.css';
-
 
 class Canvas extends Component {
 
-    penSize = (value) => {
-        console.log('pen');        
-      }
-    
-    
-      eraserSize = (props) => {
-        console.log(props.value);
-        console.log('eraser');
-      }
-    
-      downloadImage = () => {
-        p5.Image.save('photo', 'png');
-        console.log('download');
-      }
-
-    
-    x = 50;
-    y = 50;
-    constructor() {
-        super();
-        this.state = {
-            filename : '',
-        }
+  constructor() {
+    super();
+    this.state = {
+        filename : '',
+        c:null,
+        obj:null,
+        x : 50,
+        y : 50
     }
+  }
 
-    setup = (p5, canvasParentRef) =>  {
+  penSize = (value) => {
+    console.log('pen');        
+  }
     
-        p5.createCanvas((p5.windowWidth/1.4) , p5.windowHeight/1.6).parent(canvasParentRef);
-        p5.background(255);
-
-      };
     
-    //   draw = p5 => {
-    //       console.log('bruh');
-    //       p5.background(0);
-    //     p5.ellipse(this.x, this.y, 70, 70);
-    //     this.x++;
-    //   };
+  eraserSize = (props) => {
+    console.log(props.value);
+    console.log('eraser');
+  }
+    
+  downloadImage = () => {
+    this.state.obj.saveCanvas(this.state.c,'photo','jpg')
+  }
 
-      paint = p5 => {
-          console.log('painting');
-          p5.strokeWeight(10);
-          p5.stroke(0,0,0);
+  setup = (p5, canvasParentRef) =>  {
+    this.setState({
+      obj:p5,
+      c:p5.createCanvas((p5.windowWidth/1.4) , p5.windowHeight/1.6).parent(canvasParentRef)
+    })
+    p5.background(255);
+  };
+    
+  // draw = p5 => {
+  //   console.log('bruh');
+  //   p5.background(0);
+  //   p5.ellipse(this.x, this.y, 70, 70);
+  //   this.x++;
+  // };
+
+  paint = p5 => {
+    p5.strokeWeight(10);
+    p5.stroke(0,0,0);
           
-        //   console.log(r,g,b)
-          // fill(r,g,b);
-          p5.line(p5.mouseX, p5.mouseY, p5.pmouseX, p5.pmouseY);
-      }
+    // console.log(r,g,b)
+    // fill(r,g,b);
+    p5.line(p5.mouseX, p5.mouseY, p5.pmouseX, p5.pmouseY);
+  }
 
-      //
-
-    render() {
-        return(
-    <div className = "main-canvas">
+  render() {
+    return(
+      <div className = "main-canvas">
         <Sketch setup = {this.setup}  mouseDragged= {this.paint} />
-
         <div className = "bottom-buttons">
           <Button name='Pen' click = {this.penSize}/>  
           <Button name='Download' click = {this.downloadImage}/>
         </div>
-        
-    </div>
-        );
-    };
+      </div>
+    );
+  };
 }
-
 
 export default Canvas;
